ac9a8225fa486a909f1e91338ba886619f9d49f6
jrrohrer <jessie.rohrer@gmail.com>
2021-03-10 17:10:57 -0500

    add user authentication to post login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -4,12 +4,29 @@ class UsersController < ApplicationController
     get '/login' do
         #render the login form
         erb :'/users/login'
-
     end
 
+    post '/login' do
+        #finds the user
+        @user = User.find_by(username: params[:username])
+        #authenticates the user
+        if @user.authenticate(params[:password]) #returns false when the authentication fails, and returns the User instance when it passes
+            #create user session
+            session[:user_id] = @user.id
+            #redirect to the user's landing page
+            redirect to "/users/#{@user.id}"
+        else
+            #user gets an error message and redirects to login page
+        end
+    end
 
     #routes for signing up
     get '/signup' do
 
+    end
+
+    #show route - user's landing page
+    get '/users/:id' do
+        erb :'/users/show'
     end
 end
