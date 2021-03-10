24f281f4655535aab5283bd62b3ca2d9f78b1f39
jrrohrer <jessie.rohrer@gmail.com>
2021-03-10 14:37:43 -0500

    compleded initial migrations for user & yarn tables

--- db/migrate/20210310192121_create_users.rb
+++ db/migrate/20210310192121_create_users.rb
@@ -4,6 +4,7 @@ class CreateUsers < ActiveRecord::Migration
       t.string :username
       t.string :email
       t.string :password_digest
+      t.timestamps null: false
     end
   end
 end
--- db/migrate/20210310192151_create_yarns.rb
+++ db/migrate/20210310192151_create_yarns.rb
@@ -1,5 +1,12 @@
 class CreateYarns < ActiveRecord::Migration
   def change
-    
+    create_table :yarns do |t|
+      t.string :name
+      t.string :color
+      t.string :weight
+      t.string :fiber
+      t.integer :user_id
+      t.timestamps null: false
+    end
   end
 end
