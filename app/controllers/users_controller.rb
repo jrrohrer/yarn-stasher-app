562c43f19256b933de83c555b01964ad813416b0
jrrohrer <jessie.rohrer@gmail.com>
2021-03-19 14:43:53 -0400

    adding input validations for login route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -8,34 +8,36 @@ class UsersController < ApplicationController
     end
 
     post '/login' do
-        #finds the user
-        @user = User.find_by(username: params[:username])
-        #authenticates the user
-        if @user.authenticate(params[:password]) #returns false when the authentication fails, and returns the User instance when it passes
-            #create user session
-            session[:user_id] = @user.id
-            #redirect to the user's landing page
-            redirect to "/users/#{@user.id}"
+
+        if params[:username] != "" && User.exists?(username: params[:username])#checks that username is not blank and whether it exists in the
+            
+            @user = User.find_by(username: params[:username]) #@user = nil if no info is entered in the signup form AND if a username that does not exist is entered in the login form
+            if @user.authenticate(params[:password]) #returns false when the authentication fails, and returns the User instance when it passes
+                session[:user_id] = @user.id
+                redirect to "/users/#{@user.id}"
+            else
+                #error message explaining failed login attempt
+                redirect to '/login'
+            end 
         else
-            #user gets an error message and redirects back to login page
-            redirect to '/users/login'
+            #error message asking user to create an account if they don't have one already
+            redirect to '/login'
         end
+
     end
 
-    #routes for signing up
     get '/signup' do
         erb :'/users/signup'
     end
 
     post '/signup' do
-        #creates new users and persists them to the database, but only if the info in params is valid
         if params[:username] != "" && params[:email] != "" && params[:password] != ""
             @user = User.create(params)
             session[:user_id] = @user.id #signs the user in without redirecting them to the login page
             redirect "/users/#{@user.id}"
         else 
             #user gets an error message and redirects back to signup page
-            redirect to '/users/signup'
+            redirect to '/signup'
         end
 
     end
